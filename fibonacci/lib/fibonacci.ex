d2cff4be73da31f0c440f557ff6b3a2ad2f839ab
Ibrahim Moreno <kvltd00d@gmail.com>
2024-12-16 20:34:41 -0500

    added Fibonacci implementation that uses Agents for caching

---
+++ fibonacci/lib/fibonacci.ex
@@ -0,0 +1,16 @@
+defmodule Fibonacci do
+
+  alias Fibonacci.Cache, as: Cache
+
+  def fib(n) do
+    Cache.run(fn cache ->
+      cached_fib(n, cache)
+    end)
+  end
+
+  def cached_fib(n, cache) do
+    Cache.lookup(cache, n, fn ->
+      cached_fib(n-2, cache) + cached_fib(n-1, cache)
+    end)
+  end
+end
