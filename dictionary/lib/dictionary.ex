47ff1ee94c25aedff49e6c1ddd23b6b34c0f9fb2
Ibrahim Moreno <kvltd00d@gmail.com>
2025-01-06 21:20:07 -0500

    Added Runtime and moved Dictionary functions to an agent instead of an in-memory list

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -2,8 +2,10 @@ defmodule Dictionary do
   @moduledoc """
   Documentation for `Dictionary`.
   """
-  alias Dictionary.Impl.WordList, as: WordList
-  @opaque t :: WordList.t
+  # alias Dictionary.Impl.WordList, as: WordList
+  # @opaque t :: WordList.t
+  alias Dictionary.Runtime.Server, as: Server
+  @opaque t :: Server.t
   # @word_list "assets/words.txt"
   #   |> File.read!()
   #   |> String.split(~r/\n/, trim: true)
@@ -17,11 +19,11 @@ defmodule Dictionary do
       "weapon"
 
   """
-  @spec start() :: t
-  defdelegate start(), to: WordList, as: :word_list
+  @spec start_link :: { :ok, t }
+  defdelegate start_link, to: Server
 
-  @spec random_word(words :: t) :: String.t
-  defdelegate random_word(words), to: WordList, as: :random_word
+  @spec random_word(t) :: String.t
+  defdelegate random_word(words), to: Server
 
   # def random_word do
   #   @word_list
