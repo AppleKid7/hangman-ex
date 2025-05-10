edb11892b52c9c0e894590061c7ec6e5d68722ca
Ibrahim Moreno <kvltd00d@gmail.com>
2025-05-10 13:32:12 -0400

    WIP - adding web UI for game

--- b1/lib/b1_web/controllers/hangman_controller.ex
+++ b1/lib/b1_web/controllers/hangman_controller.ex
@@ -1,5 +1,6 @@
 defmodule B1Web.HangmanController do
   use B1Web, :controller
+  import Phoenix.Component, only: [to_form: 2]
 
   def home(conn, _params) do
     # The home page is often custom made,
@@ -10,7 +11,20 @@ defmodule B1Web.HangmanController do
   def new(conn, _params) do
     game = Hangman.new_game()
     tally = Hangman.tally(game)
-    put_session(conn, :game, game)
-    render conn, :game, tally: tally, layout: false
+    form = %{"guess" => ""} |> to_form(as: :make_move)
+    conn
+    |> put_session(:game, game) # Connection is immutable!
+    |> render(:game, tally: tally, layout: false, form: form)
+  end
+
+  def update(conn, params) do
+    guess = params["make_move"]["guess"]
+    tally =
+      conn 
+        |> get_session(:game)
+        |> Hangman.make_move(guess)
+    form = %{"guess" => ""} |> to_form(as: :make_move)
+    put_in(conn.params["make_move"]["guess"], "")
+    |> render(:game, tally: tally, layout: false, form: form)
   end
 end
