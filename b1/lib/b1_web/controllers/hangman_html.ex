56c77ae20a1c80609701fe67e2c0a93b4fa9a4b5
Javier <kvltd00d@gmail.com>
2025-05-13 12:14:17 -0400

    added some more styling + retry button

--- b1/lib/b1_web/controllers/hangman_html.ex
+++ b1/lib/b1_web/controllers/hangman_html.ex
@@ -8,6 +8,46 @@ defmodule B1Web.HangmanHTML do
 
   embed_templates "hangman_html/*"
 
+  def continue_or_try_again(conn, status) when status in [ :won, :lost ] do
+    path = ~p"/hangman"
+    """
+    <form action = "#{path}" method="post" style="display: inline;">
+      <input type="hidden" name="_csrf_token" value="#{Phoenix.Controller.get_csrf_token()}">
+      <button type="submit" class="btn">Try again</button>
+    </form>
+    """
+  end 
+
+  def continue_or_try_again(conn, _) do
+    path = ~p"/hangman"
+    """
+    <form action="#{path}" method="post">
+      <input type="hidden" name="_method" value="put">
+      <input type="hidden" name="_csrf_token" value="#{Phoenix.Controller.get_csrf_token()}">
+      <div class="form-text">
+        <input type="text" name="make_move[guess]">
+      </div>
+      <div>
+        <button class="btn" type="submit">Make next guess</button>
+      <div>
+    </form>
+    """
+  end
+
+  @status_fields %{
+    initializing: { "initializing", "Guess the word, a letter at a time" },
+    good_guess:   { "good-guess",   "Good guess!"},
+    bad_guess:    { "bad-guess",    "Sorry, that's a bad guess"},
+    won:          { "won",          "You won!"},
+    lost:         { "lost",         "Sorry, you lost"},
+    already_used: { "already-used", "You already used that letter"},
+  }
+
+  def move_status(status) do
+    { class, msg }  = @status_fields[status]
+    "<div class='status #{class}'>#{msg}</div>"
+  end
+
   def figure_for(0) do
     ~s{
      _____
